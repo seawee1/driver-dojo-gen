025615475f19a6fa0637d91cb3aa1dd490b48ba9
seawee1 <sebastian.rietsch@pm.me>
2023-01-09 20:49:38 +0100

    Added eval latest from checkpoint dir

--- utils/aggregate_baseline_results.py
+++ utils/aggregate_baseline_results.py
@@ -1,4 +1,5 @@
 import yaml
+import numpy as np
 from yaml.loader import SafeLoader
 import os
 import argparse
@@ -19,9 +20,16 @@ def get_dir_name(x):
 
 result_dict = dict()
 result_dict['n'] = []
-for n in [x for x in args.num.split(',')]:
-    result_dict['n'].append(n)
+for i, n in enumerate([x for x in args.num.split(',')]):
+    for k, v in result_dict:
+        if i > 0 and len(v) != i:
+            result_dict[k].append(np.NAN)
+
     path = os.path.join(args.base_path, get_dir_name(n), args.in_path)
+    if not os.path.isfile(path):
+        print(f"Could not find {path}! Skipping...")
+        continue
+    result_dict['n'].append(n)
     with open(path, 'r') as f:
         res_yaml = yaml.load(f, Loader=SafeLoader)
         for k, v in res_yaml.items():

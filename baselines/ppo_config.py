105686f6c0daab6c35deac815245252f76362ff4
seawee1 <sebastian.rietsch@pm.me>
2023-01-11 19:11:51 +0100

    Added no-threading option

--- baselines/ppo_config.py
+++ baselines/ppo_config.py
@@ -42,7 +42,7 @@ def get_config(args):
     config = config.callbacks(CustomCallback)
     config.model['framestack'] = True
     config.model['fcnet_hiddens'] = net_size
-    config['recreate_failed_workers'] = False
+    config['recreate_failed_workers'] = True
 
     print(config.model)
 
